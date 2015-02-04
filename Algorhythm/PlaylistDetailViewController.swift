4d645c57899229f42b5e3522105b03f6b14059cb
Valentin PRUGNAUD <valentin@whatdafox.com>
2015-02-04 00:36:08 -0800

    Create another view, rename master view

---
+++ Algorhythm/PlaylistDetailViewController.swift
@@ -0,0 +1,24 @@
+//
+//  PlaylistDetailViewController.swift
+//  Algorhythm
+//
+//  Created by Morgan Clayton on 2015-02-04.
+//  Copyright (c) 2015 Foxted. All rights reserved.
+//
+
+import UIKit
+
+class PlaylistDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
